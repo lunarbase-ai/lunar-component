9f4d582da1dfbf9ed09d4a681373a73edb15c220
Alex Bogatu <alex.bogatu89@yahoo.com>
2024-04-22 14:46:00 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,23 @@
+"""
+Copyright (C) 2024 Lunar AI.
+This file is part of the <COMPONENT NAME HERE> Lunar component.
+
+Author: <AUTHOR NAME HERE>
+Email: <EMAIL HERE>
+
+Notes
+-----
+This package defines <COMPONENT NAME HERE> Lunar component.
+"""
+
+from lunarverse.core.component import BaseComponent
+
+class <COMPONENT CLASS NAME>(
+    BaseComponent,
+    component_name="<COMPONENT NAME HERE>",
+    component_description="<COMPONENT DESCRIPTION HERE>",
+    input_types={},
+    output_type=,
+    component_group=,
+):
+ pass
